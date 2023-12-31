a1ba551a7f6426b1c163645397eff89b070fe0ed
Jonathan <emeraldusk@gmail.com>
2023-12-30 22:20:05 -0300

    feat(alerts): checking if cloudflared is active

--- ansible/files/scripts/alerts.sh
+++ ansible/files/scripts/alerts.sh
@@ -5,8 +5,19 @@
 TOKEN=""
 CHAT_ID=""
 
+echo "🛑 Checking if cloudflared is running"
+
+## https://www.cyberciti.biz/faq/systemd-systemctl-list-all-failed-units-services-on-linux/
+isRunning=$(systemctl is-active cloudflared)
+
+if [ "$isRunning" != "active" ]; then
+    echo "🚨 cloudflared is not running"
+    curl -X POST -H "content-type: application/json" -d "{\"chat_id\": \"$CHAT_ID\", \"text\": \"🚨 cloudflared is not running\", \"disable_notification\": true}" https://api.telegram.org/bot$TOKEN/sendMessage
+    exit 1
+fi
+
 echo "🛑 Running monitor"
-monitor=$(journalctl -u cloudflared -S "$(date -d "-1 hour" +%Y"-"%m"-"%d" "%T)" | awk '/ERR/')
+monitor=$(journalctl -u cloudflared -S "$(date -d "-1 hour" +%Y"-"%m"-"%d" "%T)" | awk '/ERR/' | tail -n 5)
 
 if [ -z "$monitor" ]; then
     echo "✅ No errors found"
