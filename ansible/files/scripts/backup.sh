10fad733856e0a7a88aa54aa84a3c14db1637e35
Jonathan <emeraldusk@gmail.com>
2024-05-24 23:30:29 -0300

    SERVER_NAME

--- ansible/files/scripts/backup.sh
+++ ansible/files/scripts/backup.sh
@@ -5,6 +5,7 @@
 
 TOKEN=""
 CHAT_ID=""
+SERVER_NAME=""
 
 if [ "$#" -eq 0 ]; then
     echo "🟩 Usage: $0 directory/*"
@@ -19,11 +20,12 @@ fi
 # https://unix.stackexchange.com/questions/24630/whats-the-best-way-to-join-files-again-after-splitting-them
 date=$(date +%F)
 tar -zcvf backup-"$date".tar.gz "$@"
-split --bytes=49M backup-"$date".tar.gz
+split --bytes=49M backup-"$date"-"$SERVER_NAME".tar.gz
 
 # https://gist.github.com/HirbodBehnam/d7a46fac29f5e1f664d467d5a05620dd
 for file in x*; do
     echo "💾 Uploading $file"
+    curl -X POST -H "content-type: application/json" -d "{\"server\": \"$SERVER_NAME\", \"Date: $date\n"}" https://api.telegram.org/bot"$TOKEN"/sendMessage
     curl -X POST -H "content-type: application/json" -d "{\"chat_id\": \"$CHAT_ID\", \"text\": \"Date: $date\nFile: $file\", \"disable_notification\": true}" https://api.telegram.org/bot"$TOKEN"/sendMessage
     curl -X POST -H "content-type: multipart/form-data" -F document=@"$file" -F chat_id="$CHAT_ID" https://api.telegram.org/bot"$TOKEN"/sendDocument
 done
