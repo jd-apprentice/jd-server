fa00006f408819229064e66e33affd60396328e8
jd-apprentice <contacto@jonathan.com.ar>
2024-09-05 00:24:42 -0300

    chore: codacy quality gate

--- performance/k6.js
+++ performance/k6.js
@@ -17,6 +17,14 @@ export const options = {
   }
 };
 
+/**
+ * @usage k6 cloud run performance/k6.js --env PROJECT_ID=<project-id> --env TARGET_URL=<target-url>
+ * @environment VUS (default: 10)
+ * @environment TARGET_URL (default: ...)
+ * @environment PROJECT_ID (default: ...)
+ * @environment DURATION (default: 30s)
+ * @description K6 performance test for cloud run
+ */
 export default function() {
  const url = __ENV.TARGET_URL;
 
