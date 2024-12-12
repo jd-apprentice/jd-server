413442ec29c75760530e1fcf5e9c3ac5cf781ad3
jd-apprentice <contacto@jonathan.com.ar>
2024-12-11 21:48:22 -0300

    chore: update arch backup script

---
+++ ansible/files/home/backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+## Usage: backup.sh [mode]
+## EDIT: rsyncignore
+
+BASE_NAME="storage"
+DOMAIN=".local"
+MODE=${1:-backup}
+USER=$(whoami)
+REMOTE_PATH="$HOME/arch"
+LOCAL_PATH="$HOME/"
+
+if [[ "$MODE" == "restore" ]]; then
+    REMOTE_DEST="${USER}@${BASE_NAME}${DOMAIN}:${REMOTE_PATH}"
+
+    rsync -aAXHvr --verbose --filter="merge $HOME/.config/rsyncignore" -e ssh "$REMOTE_DEST" "$LOCAL_PATH"
+    echo "Completed restore"
+    exit 0
+fi
+
+REMOTE_DEST="${USER}@${BASE_NAME}${DOMAIN}:${REMOTE_PATH}"
+
+
+if [[ "$MODE" == "backup" ]]; then
+    rsync -aAXHvr --verbose --filter="merge $HOME/.config/rsyncignore" -e ssh "$LOCAL_PATH" "$REMOTE_DEST"
+fi
+
+if [ ! $? -eq 0 ]; then
+    echo "Err while doing backup for $HOST. Skipping..."
+    exit 1
+fi
+
+echo "Completed backup for $BASE_NAME$DOMAIN"
