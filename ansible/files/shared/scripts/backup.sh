489db7f1b43ae9686121b4d3b5bdd5e42e9b098d
jd-apprentice <contacto@jonathan.com.ar>
2024-12-25 19:23:34 -0300

    chore: update backup script for servers

--- ansible/files/shared/scripts/backup.sh
+++ ansible/files/shared/scripts/backup.sh
@@ -5,30 +5,19 @@
 
 TOKEN=""
 CHAT_ID=""
-SERVER_NAME=$(hostname)
+SERVER_NAME="ingress"
 
-if [ "$#" -eq 0 ]; then
-    echo "🟩 Usage: $0 directory/*"
-    exit 1
-fi
-
-if ! [ -x "$(command -v curl)" ]; then
-    echo "❌ Error: curl is not installed." >&2
-    exit 1
-fi
-
-# https://unix.stackexchange.com/questions/24630/whats-the-best-way-to-join-files-again-after-splitting-them
 date=$(date +%F)
-tar -zcvf backup-"$date"-"$SERVER_NAME".tar.gz "$@"
-split --bytes=49M backup-"$date"-"$SERVER_NAME".tar.gz
+sudo tar --exclude='.env' -zcvf backup-"$date"-"$SERVER_NAME".tar.gz $@
+split --bytes=49M backup-"$date"-"$SERVER_NAME".tar.gz part-
 
-# https://gist.github.com/HirbodBehnam/d7a46fac29f5e1f664d467d5a05620dd
-for file in x*; do
-    echo "💾 Uploading $file"
-    curl -X POST -H "content-type: application/json" -d "{\"chat_id\": \"$CHAT_ID\", \"text\": \"Date: $date\nFile: $file\nServer: $SERVER_NAME\", \"disable_notification\": true}" https://api.telegram.org/bot"$TOKEN"/sendMessage
-    curl -X POST -H "content-type: multipart/form-data" -F document=@"$file" -F chat_id="$CHAT_ID" https://api.telegram.org/bot"$TOKEN"/sendDocument
+for file in part-*; do
+    mv "$file" "${file}-${SERVER_NAME}"
 done
 
-rm backup-"$date"-"$SERVER_NAME".tar.gz x*
-notify 192.168.0.242 8588 token backup "created a backup" 1
-echo "🚀 Backup completed!"
+for file in part-*-"$SERVER_NAME"; do
+    curl -X POST -H "content-type: application/json" -d "{\"chat_id\": \"$CHAT_ID\", \"text\": \"Date: $date\nFile: $file\nServer: $SERVER_NAME\", \"disable_notification\": true}" https://api.telegram.org/bot$TOKEN/sendMessage
+    curl -X POST -H "content-type: multipart/form-data" -F document=@"$file" -F chat_id=$CHAT_ID https://api.telegram.org/bot$TOKEN/sendDocument
+done
+
+notify 192.168.88.251 8588 token title "[ingress] Created a backup for folder /www" 1
